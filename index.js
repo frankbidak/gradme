f37711fde1b71dcb3807f4e3b5b0db21ea146df6
frankbidak <frankbidak@gmail.com>
2022-10-08 21:45:49 -0700

    initial web server code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+var http = require('http');
+var hostname = 'localhost';
+var port = 8000;
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
